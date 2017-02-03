d61fc615719c422fa08053e7a33a39fb54ffc2a8
marcopizzichemi <marco.pizzichemi@gmail.com>
2017-02-03 13:14:56 +0100

    input from config file completed

--- userparams.c
+++ userparams.c
@@ -93,93 +93,47 @@ void SetUserParams(UserParams_t *Params, ConfigFile& config)
     Params->PostTrigger.push_back((uint16_t)strtol(PostTrigger_f[i].c_str(), NULL, 0));
   }
 
-  // Parameters check
-  std::cout << std::endl;
-  std::cout << "//////////////////////////////////" << std::endl;
-  std::cout << "//      Config Parameters       //" << std::endl;
-  std::cout << "//////////////////////////////////" << std::endl;
-  std::cout << std::endl;
-  for(unsigned int i = 0 ; i < Params->ConnectionType.size() ; i++)
-  {
-    std::cout << "********** Digitizer "            << i << "************" << std::endl;
-    std::cout << "ConnectionType\t\t= "             << Params->ConnectionType[i] << "\t\t" << "// 0 = USB, 1 = OpticalLink"  << std::endl
-              << "LinkNum\t\t\t= "                  << Params->LinkNum[i] << std::endl
-              << "ConetNode\t\t= "                  << Params->ConetNode[i] << std::endl
-              << "BaseAddress\t\t= "                << "0x" << std::hex << Params->BaseAddress[i] << std:: dec << std::endl
-              << "FastTriggerThreshold\t= "         << Params->FastTriggerThreshold[i] << std::endl
-              << "FastTriggerOffset\t= "            << Params->FastTriggerOffset[i] << std::endl
-              << "DCoffset\t\t= "                   << "0x"<< std::hex << Params->DCoffset[i]  << std:: dec << std::endl
-              << "ChannelThreshold\t= "             << Params->ChannelThreshold[i] << std::endl
-              << "TRThreshold\t\t= "                << Params->TRThreshold[i] << std::endl
-              << "ChannelPulseEdge\t= "             << Params->ChannelPulseEdge[i] << "\t\t" << "// 0 = Rising Edge, 1 = Falling Edge" << std::endl
-              << "PostTrigger\t\t= "                << Params->PostTrigger[i] << std::endl;
-    // Params->ConnectionType[i] = (CAEN_DGTZ_ConnectionType) ConnectionType[i];
-    // Params->LinkNum[i] = LinkNum[i];
-    // Params->ConetNode[i] = ConetNode[i];
-    // Params->BaseAddress[i] = BaseAddress[i];
-  }
 
 
-
-  // Params->ConnectionType[0]	= CAEN_DGTZ_OpticalLink; //CAEN_DGTZ_USB;
-  // Params->ConnectionType[1]	= CAEN_DGTZ_OpticalLink; //CAEN_DGTZ_USB;
-
-	// Params->LinkNum[0]		= 0;
-	// Params->ConetNode[0]		= 0;
-	// Params->BaseAddress[0]		= 0x32100000; //Master board
-
-
-	// Params->LinkNum[1]		= 0;
-	// Params->ConetNode[1]		= 1;
-	// Params->BaseAddress[1]		= 0x33100000; //Slave board
+  //SINGLE VALUES
+  //read config file
+  int TriggerEdge     = config.read<int>("TriggerEdge",1);       // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int RecordLength    = config.read<int>("RecordLength",1024);   // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int MatchingWindow  = config.read<int>("MatchingWindow",200);  // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int IOlevel         = config.read<int>("IOlevel",0);           // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int TestPattern     = config.read<int>("TestPattern",0);       // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int DRS4Frequency   = config.read<int>("DRS4Frequency",0);     // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int StartMode       = config.read<int>("StartMode",0);         // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+  int EnableLog       = config.read<int>("EnableLog",1);         // Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
 
 	// TR and CHANNEL SETTINGS
     // NIM signal on TR:	DC_OFFSET 32768 	TRIGGER_THRESHOLD 20934
     // NIM/2 signal on TR:	DC_OFFSET 32768 	TRIGGER_THRESHOLD 23574
 	// AC signal on TR:		DC_OFFSET 32768		TRIGGER_THRESHOLD 26214
-	// +2V signal on TR: 	DC_OFFSET 43520		TRIGGER_THRESHOLD 26214
-	// Params->RefChannel[0]			=16;		// Channel(or group?) of the Master used for the acquisition
-	// Params->FastTriggerThreshold[0]	= 20934;	// TR Trigger threshold (for fast triggering)
-	// Params->FastTriggerOffset[0]	= 32768;   // TR Offset adjust (DAC value)
-	// Params->DCoffset[0]				= 0x7FFF;   // input DC offset adjust (DAC value)
-	// Params->PostTrigger[0]			= 0;		// Post trigger in percent of the acquisition window
-	// Edge Setting
-	// Params->ChannelThreshold[0]		= 1500;		// Threshold for Time Pulse Calculation
-	// Params->ChannelPulseEdge[0]		= CAEN_DGTZ_TriggerOnFallingEdge; // CAEN_DGTZ_TriggerPolarity_t
-	// Params->TRThreshold[0]			= 1500;		// Threshold for Time Pulse Calculation
+  // +2V signal on TR: 	DC_OFFSET 43520		TRIGGER_THRESHOLD 26214
 
-	// Params->RefChannel[1]			= 16;		// Group of the Slave used for the acquisition
-	// Params->FastTriggerThreshold[1]	= 20934;		// TR Trigger threshold (for fast triggering)
-	// Params->FastTriggerOffset[1]	= 32768;   // TR Offset adjust (DAC value)
-	// Params->DCoffset[1]				= 0x7FFF;   // input DC offset adjust (DAC value)
-	// Params->PostTrigger[1]			= 0;		// Post trigger in percent of the acquisition window
-	// Edge Setting
-	// Params->ChannelThreshold[1]		= 1500;		// Threshold for Time Pulse Calculation
-	// Params->ChannelPulseEdge[1]		= CAEN_DGTZ_TriggerOnFallingEdge;
-	// Params->TRThreshold[1]			= 1500;		// Threshold for Time Pulse Calculation
-
-	// Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge, CAEN_DGTZ_TriggerOnFallingEdge)
-	Params->TriggerEdge			= CAEN_DGTZ_TriggerOnFallingEdge;
+	// Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)
+	Params->TriggerEdge			= (CAEN_DGTZ_TriggerPolarity_t) TriggerEdge;
 
 	// Number of samples in the acquisition windows (Valid Options: 1024, 520, 256 and 136)
-	Params->RecordLength		= 1024;
+	Params->RecordLength		= RecordLength;
 
 	// Max. distance between the trigger time tags in order to consider a valid coincidence
-	Params->MatchingWindow		= 200;
+	Params->MatchingWindow		= MatchingWindow;
 
 	// Front Panel LEMO I/O level (NIM or TTL). Options: CAEN_DGTZ_IOLevel_NIM, CAEN_DGTZ_IOLevel_TTL
-	Params->IOlevel				= CAEN_DGTZ_IOLevel_NIM;
+	Params->IOlevel				= (CAEN_DGTZ_IOLevel_t) IOlevel;
 
 	// Internal Test Pattern (triangular wave replacing ADC data): 0=disabled, 1=enabled
-	Params->TestPattern			= 0;
+	Params->TestPattern			= TestPattern;
 
 	// DRS4SamplingFrequency. Options: CAEN_DGTZ_DRS4_5GHz, CAEN_DGTZ_DRS4_2_5GHz,CAEN_DGTZ_DRS4_1GHz
-	Params->DRS4Frequency		= CAEN_DGTZ_DRS4_5GHz;
+	Params->DRS4Frequency		= (CAEN_DGTZ_DRS4Frequency_t) DRS4Frequency;
 
 	// ***************************************************************************************************
 	// Start Mode. Options: START_SW_CONTROLLED, START_HW_CONTROLLED
 	// ***************************************************************************************************
-	Params->StartMode = START_SW_CONTROLLED;
+	Params->StartMode =  StartMode;
 
 	// ***************************************************************************************************
 	// Sync Mode: INDIVIDUAL_TRIGGER_SIN_TRGOUT ()
@@ -206,7 +160,7 @@ void SetUserParams(UserParams_t *Params, ConfigFile& config)
 	// ***************************************************************************************************
 	// Log Enabling
 	// ***************************************************************************************************
-	Params->EnableLog			= 1;
+	Params->EnableLog			= EnableLog;
 
 	// ***************************************************************************************************
 	// Parameters for the time distribution histogram
@@ -215,6 +169,40 @@ void SetUserParams(UserParams_t *Params, ConfigFile& config)
 	// Params->HistoBinSize		= 0.005;		// Bin size in ns
 	// Params->HistoOffset			= -4.0;	// Lower value of the histogram in ns
 
+
+
+  // Parameters check
+  std::cout << std::endl;
+  std::cout << "//////////////////////////////////" << std::endl;
+  std::cout << "//      Config Parameters       //" << std::endl;
+  std::cout << "//////////////////////////////////" << std::endl;
+  std::cout << std::endl;
+  for(unsigned int i = 0 ; i < Params->ConnectionType.size() ; i++)
+  {
+    std::cout << "********** Digitizer "            << i << "************" << std::endl;
+    std::cout << "ConnectionType\t\t= "             << Params->ConnectionType[i]                  << "\t\t" << "# 0 = USB, 1 = OpticalLink"  << std::endl
+              << "LinkNum\t\t\t= "                  << Params->LinkNum[i]                         << "\t\t" << "# Link number for this digitizer" << std::endl
+              << "ConetNode\t\t= "                  << Params->ConetNode[i]                       << "\t\t" << "# Conet Node" << std::endl
+              << "BaseAddress\t\t= "                << "0x" << std::hex << Params->BaseAddress[i] << std:: dec << "\t" << "# Base address for this digitizer" << std::endl
+              << "FastTriggerThreshold\t= "         << Params->FastTriggerThreshold[i]            << "\t\t" << "# TR Trigger threshold (for fast triggering)" << std::endl
+              << "FastTriggerOffset\t= "            << Params->FastTriggerOffset[i]               << "\t\t" << "# TR Offset adjust (DAC value)" << std::endl
+              << "DCoffset\t\t= "                   << "0x"<< std::hex << Params->DCoffset[i]     << std:: dec << "\t" << "# input DC offset adjust (DAC value)" << std::endl
+              << "ChannelThreshold\t= "             << Params->ChannelThreshold[i]                << "\t\t" << "# Threshold for Time Pulse Calculation" << std::endl
+              << "TRThreshold\t\t= "                << Params->TRThreshold[i]                     << "\t\t" << "# Threshold for Time Pulse Calculation" << std::endl
+              << "ChannelPulseEdge\t= "             << Params->ChannelPulseEdge[i]                << "\t\t" << "# 0 = Rising Edge, 1 = Falling Edge"  << std::endl
+              << "PostTrigger\t\t= "                << Params->PostTrigger[i]                     << "\t\t" << "# Post trigger in percent of the acquisition window" << std::endl;
+
+
+  }
+  std::cout << "*****  Global parameters *****" << std::endl;
+  std::cout << "TriggerEdge\t\t= "			            << Params->TriggerEdge		<< "\t\t" << "# Trigger edge (CAEN_DGTZ_TriggerOnRisingEdge = 0, CAEN_DGTZ_TriggerOnFallingEdge = 1)" << std::endl
+            << "RecordLength\t\t= "		              << Params->RecordLength		<< "\t\t" << "# Number of samples in the acquisition windows (Valid Options: 1024, 520, 256 and 136)" << std::endl
+            << "MatchingWindow\t\t= "			          << Params->MatchingWindow	<< "\t\t" << "# Max. distance between the trigger time tags in order to consider a valid coincidence" << std::endl
+            << "IOlevel\t\t\t= "		                << Params->IOlevel			  << "\t\t" << "# Front Panel LEMO I/O level (NIM or TTL). Options: CAEN_DGTZ_IOLevel_NIM = 0, CAEN_DGTZ_IOLevel_TTL = 1" << std::endl
+            << "TestPattern\t\t= "		              << Params->TestPattern		<< "\t\t" << "# Internal Test Pattern (triangular wave replacing ADC data): 0 = disabled, 1 = enabled" << std::endl
+            << "DRS4Frequency\t\t= "		            << Params->DRS4Frequency	<< "\t\t" << "# DRS4SamplingFrequency. Options: CAEN_DGTZ_DRS4_5GHz = 0, CAEN_DGTZ_DRS4_2_5GHz = 1 ,CAEN_DGTZ_DRS4_1GHz = 2" << std::endl
+            << "StartMode\t\t= "		                << Params->StartMode      << "\t\t" << "# Start Mode. Options: START_SW_CONTROLLED = 0, START_HW_CONTROLLED = 1" << std::endl
+            << "EnableLog\t\t= "		                << Params->EnableLog	    << "\t\t" << "# Log Enabling (1 = yes, 0 = no)" << std::endl ;
 }
 
 void SetDefaultParams(UserParams_t *Params)
